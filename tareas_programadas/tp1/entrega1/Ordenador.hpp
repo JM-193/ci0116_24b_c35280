528014367c5938ae3c0c8dcb90a394dda0c61016
Jose Manuel Mora Z <jose.morazuniga@ucr.ac.cr>
2024-09-16 14:28:43 -0600

    TP1.1: added ImprimirDatosDeTarea() and Rule of 5

--- tareas_programadas/tp1/entrega1/Ordenador.hpp
+++ tareas_programadas/tp1/entrega1/Ordenador.hpp
@@ -2,21 +2,31 @@
 
 class Ordenador {
     private:
-    // Defina aqui los metodos auxiliares de los algoritmos de ordenamiento solamente.
-    // Puede definir cuantos metodos quiera.
-    
+    // Defina aquí los métodos auxiliares de los algoritmos de ordenamiento
+    // solamente.
+    // Puede definir cuantos métodos quiera.
+
     public:
     Ordenador() = default;
     ~Ordenador() = default;
+
+    Ordenador(Ordenador& o) = delete;
+    Ordenador(Ordenador&& o) = delete;
+    Ordenador& operator=(Ordenador& o) = delete;
+    Ordenador& operator=(Ordenador&& o) = delete;
+
     /* Nota:
-     - Si no planea implementar algunos de los métodos de ordenamiento, no los borre.
-     - Simplemente déjelos con el cuerpo vacío para evitar errores de compilación, ya
-       que se ejecutará el mismo main para todas las tareas.
-     - Recuerde hacer uso de programación defensiva y documentar los métodos con formato Doxygen, por ejemplo.
+     - Si no planea implementar algunos de los métodos de ordenamiento, no los
+       borre.
+     - Simplemente déjelos con el cuerpo vacío para evitar errores de
+       compilación, ya que se ejecutará el mismo main para todas las tareas.
+     - Recuerde hacer uso de programación defensiva y documentar los métodos
+       con formato Doxygen, por ejemplo.
      - No cambié la firma de los métodos de la clase Ordenador.
      - No lance excepciones para el manejo de errores.
-       Ante un error, basta con que el método no modifique el arreglo original y que no cause la caída del programa.
-    */ 
+       Ante un error, basta con que el método no modifique el arreglo original
+       y que no cause la caída del programa.
+    */
     void ordenamientoPorSeleccion(int *A, int n) const;
     void ordenamientoPorInsercion(int *A, int n) const;
     void ordenamientoPorMezcla(int *A, int n) const;
@@ -26,12 +36,23 @@ class Ordenador {
 
     /**
      * @brief Retorna un std::string con los datos de la tarea.
-     * 
-     * Este método devuelve una cadena de texto que contiene el carné, nombre y tarea.
-     * 
+     *
+     * Este método devuelve una cadena de texto que contiene el carné, nombre
+     * y tarea.
+     *
      * @return std::string Una cadena de texto con los datos de la tarea.
      */
     constexpr const char* datosDeTarea() const {
-      return "Carné: 12345, Nombre: AA, Tarea 1"
+      return "Carné: 12345, Nombre: AA, Tarea 1";
+    }
+
+    /**
+     * @brief Retorna el string solicitado con los datos del estudiante y
+     * la tarea.
+     *
+     * @return constexpr const char* String con los datos solicitados.
+     */
+    constexpr const char* ImprimirDatosDeTarea() const {
+      return "Carné: C35280, Nombre: José Manuel Mora Z, Tarea 1 Etapa 1";
     }
 };
