827151f4b9ac8552c0eb733d4425b13e9bd7000f
Jose Manuel Mora Z <jose.morazuniga@ucr.ac.cr>
2024-11-03 23:47:20 -0600

    Tp2: Testing for SLL

--- tareas_programadas/tp2/source/SinglyLinkedList.hpp
+++ tareas_programadas/tp2/source/SinglyLinkedList.hpp
@@ -40,7 +40,7 @@ class SLListNode {
 
   /// @brief Get the key of the node
   /// @return The key of the node
-  DataType getKey() const { this->key; }
+  DataType getKey() const { return this->key; }
 
   /// @brief Get the next node
   /// @return The next node
@@ -87,6 +87,7 @@ class SLList {
       delete current;
       current = next;
     }
+    this->nil = nullptr;
   }
 
   /// @brief Inserts a new element into the beginning of list
@@ -98,29 +99,53 @@ class SLList {
 
   /// @brief Searches for a value in the list
   /// @param value Value to be searched
-  /// @param prev Pointer to the previous node if needed
+  /// @param prev Reference to return the previous node by parameter
   /// @return The first node with the value or nullptr if not found
   SLListNode<DataType>* search(const DataType& value,
-      SLListNode<DataType>*& prev = nullptr) const {
+      SLListNode<DataType>*& prev) const {
+    // Node pointers
+    prev = nullptr;
     SLListNode<DataType>* current = this->nil;
+    // Search for the value
     while (current != nullptr && current->getKey() != value) {
+      // Move to the next node, updating prev as well
       prev = current;
       current = current->getNext();
     }
+    // Return the node or nullptr if not found
     return current;
+  }
+
+  /// @brief Searches for a value in the list
+  /// @param value Value to be searched
+  /// @return The first node with the value or nullptr if not found
+  SLListNode<DataType>* search(const DataType& value) const {
+    // Use the search function with a nullptr prev
+    SLListNode<DataType>* prev = nullptr;
+    return search(value, prev);
   }
 
   /// @brief Removes every occurrence of a value in the list
   /// @param value Value to be removed
   void remove(const DataType& value) {
-    while (true) {
-      SLListNode<DataType>* prev = nullptr;
-      SLListNode<DataType>* current = this->search(value, prev);
+    // Node pointers
+    SLListNode<DataType>* prev = nullptr;
+    SLListNode<DataType>* current = this->nil;
 
-      if (current == nullptr) {
-        break;
+    // Search for the value
+    while (current) {
+      // If the value is found, remove it
+      if (current->getKey() == value) {
+        // Node to remove
+        SLListNode<DataType>* nodeToRemove = current;
+        // Update current
+        current = current->getNext();
+        // Remove the node
+        this->remove(nodeToRemove, prev);
       } else {
-        this->remove(current, prev);
+        // Move to the next node, updating prev as well
+        prev = current;
+        current = current->getNext();
       }
     }
   }
@@ -131,32 +156,17 @@ class SLList {
   /// @param prev Previous node if needed
   void remove(SLListNode<DataType>* node,
       SLListNode<DataType>* prev = nullptr) {
-    // If the node is nullptr, there's nothing to remove
+    // If the node is null, return
     if (node == nullptr) return;
-
-    SLListNode<DataType>* current = this->nil;
-    if (prev) {
-      // If prev is given, we can remove the node directly
-      current = node;
+    if (!prev) {
+      // If there's no previous node, we are removing the first element
+      this->nil = node->getNext();
     } else {
-      // If no prev is given, search for the node
-      while (current != nullptr && current != node) {
-        prev = current;
-        current = current->getNext();
-      }
+      // If there's a previous node, we can remove the current node
+      prev->setNext(node->getNext());
     }
-
-    // Node removal
-    if (current != nullptr) {
-      if (prev == nullptr) {
-        // If we found it and prev is nullptr, then it is the first element
-        this->nil = node->getNext();
-      } else {
-        // Connect the previous node with the next node
-        prev->setNext(node->getNext());
-      }
-      delete node;
-    }
+    // Delete the node
+    delete node;
   }
 
  public:
--- tareas_programadas/tp2/source/TestDataStructures.cpp
+++ tareas_programadas/tp2/source/TestDataStructures.cpp
@@ -10,13 +10,15 @@
 #include "RedBlackTree.hpp"
 #include "SinglyLinkedList.hpp"
 
+constexpr std::size_t runs = 3;
 constexpr std::size_t arr_len = 1000000;
 constexpr std::size_t min = 0;
 constexpr std::size_t max = 3 * arr_len;
-constexpr std::size_t search = 10000;
-constexpr std::size_t remove = 10000;
+constexpr std::size_t search_len = 10000;
+constexpr std::size_t remove_len = 10000;
 
-void generateRandomArray(std::array<int, arr_len>& arr) {
+template<std::size_t len>
+void generateRandomArray(std::array<int, len>& arr) {
   std::random_device rd;
   std::mt19937 generator(rd());
   std::uniform_int_distribution<int> distribution(min, max);
@@ -26,39 +28,89 @@ void generateRandomArray(std::array<int, arr_len>& arr) {
   }
 }
 
-int main(int argc, char* argv[]) {
+void testSLL(SLList<int>& sll, bool random,
+    std::array<int, arr_len>& insertArr,
+    std::array<int, search_len>& searchArr,
+    std::array<int, remove_len>& removeArr) {
+  // Insertion
+  generateRandomArray(insertArr);
+  auto startTimeI = std::chrono::high_resolution_clock::now();
+  if (random) {
+    for (const auto& value : insertArr) {
+      sll.insert(value);
+    }
+  } else {
+    for (std::size_t value = 0; value < arr_len; ++value) {
+      sll.insert(value);
+    }
+  }
+  auto endTimeI = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double, std::milli> durationI = endTimeI - startTimeI;
+  std::cout << "\t\tInsertion: \t" << durationI.count() << " ms" << std::endl;
+
+  // Search
+  generateRandomArray(searchArr);
+  auto startTimeS = std::chrono::high_resolution_clock::now();
+  for (const auto& value : searchArr) {
+    sll.search(value);
+  }
+  auto endTimeS = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double, std::micro> durationS = endTimeS - startTimeS;
+  std::cout << "\t\tSearch: \t" << durationS.count() << " μs" << std::endl;
+
+  // Removal
+  generateRandomArray(removeArr);
+  auto startTimeR = std::chrono::high_resolution_clock::now();
+  for (const auto& value : removeArr) {
+    sll.remove(value);
+  }
+  auto endTimeR = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> durationR = endTimeR - startTimeR;
+  std::cout << "\t\tRemoval: \t" << durationR.count() << " s" << std::endl;
+
+  // Clear the list
+  sll.clear();
+}
+
+int main() {
   SLList<int> sll;
   BSTree<int> bst;
   RBTree<int> rbt;
   ChainedHashTable<int> cht(arr_len);
 
-  std::array<int, arr_len> arr;
+  std::array<int, arr_len> insertArr;
+  std::array<int, search_len> searchArr;
+  std::array<int, remove_len> removeArr;
 
-  std::cout << "Singly Linked List: Random" << std::endl;
-  generateRandomArray(arr);
-  for (const auto& value : arr) {
-    sll.insert(value);
+  std::cout << "\nSingly Linked List: Random" << std::endl;
+  for (std::size_t i = 0; i < runs; ++i) {
+    std::cout << "\tRun " << i + 1 << ":" << std::endl;
+    testSLL(sll, /* random */ true, insertArr, searchArr, removeArr);
   }
 
-  std::cout << "Singly Linked List: Sorted" << std::endl;
+
+  std::cout << "\nSingly Linked List: Sorted" << std::endl;
+  for (std::size_t i = 0; i < runs; ++i) {
+    std::cout << "\tRun " << i + 1 << ":" << std::endl;
+    testSLL(sll, /* random */ false, insertArr, searchArr, removeArr);
+  }
+
+  std::cout << "\nBinary Search Tree: Random" << std::endl;
 
 
-  std::cout << "Binary Search Tree: Random" << std::endl;
+  std::cout << "\nBinary Search Tree: Sorted" << std::endl;
 
 
-  std::cout << "Binary Search Tree: Sorted" << std::endl;
+  std::cout << "\nRed-Black Tree: Random" << std::endl;
 
 
-  std::cout << "Red-Black Tree: Random" << std::endl;
+  std::cout << "\nRed-Black Tree: Sorted" << std::endl;
 
 
-  std::cout << "Red-Black Tree: Sorted" << std::endl;
+  std::cout << "\nChained Hash Table: Random" << std::endl;
 
 
-  std::cout << "Chained Hash Table: Random" << std::endl;
-
-
-  std::cout << "Chained Hash Table: Sorted" << std::endl;
+  std::cout << "\nChained Hash Table: Sorted" << std::endl;
 
 
   return EXIT_SUCCESS;
